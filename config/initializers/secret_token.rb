4d2f3a84497a4936f77b365aabb4b0c40557540c
daino3 <dainovu3@gmail.com>
2014-02-05 13:57:14 -0600

    initial commit - messing around with refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newcms::Application.config.secret_token = '68d5a6ff469748f05f029c47652905294b0d126676e167d61f32c684810464df95d1b1c8923b63620121d3cf27e65540c58b6a60c94b6cecdd458f2f7718690e'
